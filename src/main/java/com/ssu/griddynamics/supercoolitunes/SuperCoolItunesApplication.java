f27ee933809e09d2788c26ae121f3f12c55480b4
victor <victor_artemov@outlook.com>
2018-11-10 22:24:53 +0400

    Initial structure of the project

---
+++ src/main/java/com/ssu/griddynamics/supercoolitunes/SuperCoolItunesApplication.java
@@ -0,0 +1,12 @@
+package com.ssu.griddynamics.supercoolitunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCoolItunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperCoolItunesApplication.class, args);
+	}
+}
